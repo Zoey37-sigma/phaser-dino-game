7daef0d65390355cb94eb66015a3fa8b88004e99
Zoey37-sigma <zoey_teo_jia_xin@students.edu.sg>
2025-03-26 08:58:01 +0000

    Until obstacles

--- game.js
+++ game.js
@@ -26,13 +26,21 @@ function preload() {
     this.load.spritesheet("dino", "assets/dino-idle.png", {frameWidth:88, frameHeight:94});
     this.load.image("ground", "assets/ground.png");
     this.load.image("cloud", "assets/cloud.png");
-    
-    
+    for(let i =0;i<6,i++){
+    this.load.image(`obstacle${i}`,`asets/cactuses_${i}.png`)
 }
 
 function create() {
-    this.physics.add.sprite(200,200,"dino").setOrigin(0);
-    this.ground = this.add.tileSprite(0,300,1000,30,"ground").setOrigin(0,1);
+   this.player =this.physics.add.sprite(200,200,"dino").setOrigin(0,1)
+    .setCollideWorldBounds(true)
+    .setBodySize(44,92)
+    .setGravityY(5000);
+        this.ground = this.add.tileSprite(0,300,1000,30,"ground").setOrigin(0,1);
+        this.groundCollider= this.physics.add.staticSprite(0,300,"ground").setOrigin(0,1);
+        this.groundCollider.body.setSize(1000,30);
+
+        this.physics.add.collider(this.player,this.groundCollider);
+
     this.clouds = this.add.group();
     this.clouds = this.clouds.addMultiple(
         [this.add.image(200,100, "cloud"),
@@ -41,9 +49,13 @@ function create() {
         ]
     );
     this.gameSpeed =5;
-}
+    this.obstacles=this.physics.add.group({
+    allowGravity:false
+});
 
 function update() {
 this.ground.tilePositionX +=this.gameSpeed;
 //as game progresses, every time update runs, ground moves by fixed unit to the left 
+const num=Math.floor(Math.random()*6)+1;
+this.obstacles.create(750,220,`obstacle${num}`).setOrigin(0);
 }
